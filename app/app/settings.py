1187122dffedfa33c59061d0d110a0f10f15ce07
Nyyir <nyyirs@gmail.com>
2020-01-05 13:59:45 +0400

    Configured for deployment.

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rmkvtio&b$xvdene(t2%0-ff#$3+pqm!0vnh2b0ul-7w=u8xbe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static/'
